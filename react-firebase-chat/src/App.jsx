10a289e3b7d6734a7dea6e333545492383abe24d
yaelgalili <yael.galili@rayzoneg.com>
2025-02-05 17:37:20 +0200

    Login page design

--- react-firebase-chat/src/App.jsx
+++ react-firebase-chat/src/App.jsx
@@ -1,13 +1,22 @@
 import List from "./components/List/List";
 import Chat from "./components/Chat/Chat";
 import Detail from "./components/Detail/Detail";
+import Login from "./components/Login/Login";
 
 const App = () => {
+  const user = false;
+
   return (
     <div className="Container">
-      <List />
-      <Chat />
-      <Detail />
+      {
+        user ? (
+          <>
+            <List />
+            <Chat />
+            <Detail />
+          </>
+        ) : (<Login />)
+      }
     </div>
   );
 };
